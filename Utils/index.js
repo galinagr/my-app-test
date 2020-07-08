e18bbd269cb93ff3ffc76dc614c8832155579aaa
Galina Gryaznova <gagr@ciklum.com>
2020-07-08 20:27:51 +0300

    Headline tests, propTypes, check-prop-types usage

--- Utils/index.js
+++ Utils/index.js
@@ -1,5 +1,10 @@
+import checkPropTypes from 'check-prop-types'
+import Headline from "../src/components/headline"
 
 export const findByAttr = (component, attr) => {
     return component.find(`[data-test='${attr}']`)
 }
 
+export const checkProps = (component, expectedProps) => {
+    return checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
+}
