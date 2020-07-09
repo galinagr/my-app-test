30008d8735cf169ce77647fb6f3c69b10a0821f3
Galina Gryaznova <gagr@ciklum.com>
2020-07-09 18:44:50 +0300

    Post reducers and tests have been added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {store} from './createStore'
 import App from './App';
 
 
-ReactDOM.render(<App />,
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,
   document.getElementById('root')
 )
 
